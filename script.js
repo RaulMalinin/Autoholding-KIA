a380f0076e2ec83b67d9e1a7a14a419c971a4199
Raul <raulmalinin@gmail.com>
2019-10-23 11:48:50 +0300

    Правки1

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-/*$('.models').click(function () {
-        $('.models__list-main', this).css('display','flex');
-});*/
-  
-  /*$('.models').touch(function () {
-    $('.models__list-main', this).css('display','flex');
-  });*/
 
 $('.models').on('click touchstart', function () {
         $('.models__list-main', this).css('display','flex');
 });
 
 
-$(document).ready(function(){
-	$(".modalbox").fancybox();
-	$("#f_contact").submit(function(){ return false; });
-	$("#f_send").on("click", function(){
-		
-		// тут дальнейшие действия по обработке формы
-		// закрываем окно, как правило делать это нужно после обработки данных
-		$("#f_contact").fadeOut("fast", function(){
-			$(this).before("<p><strong>Ваше сообщение отправлено!</strong></p>");
-			setTimeout("$.fancybox.close()", 1000);
-		});
-	});
-});
